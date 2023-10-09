04b8cbf8acac54fa1de530abe4fdba978d894fdb
ibadurrahmaAlHadi <extremegilang@gmail.com>
2023-10-09 10:37:44 +0700

    adding bootstrap

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -8,8 +8,9 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="/css/main.css" rel="stylesheet" >
+    <!-- <link href="/css/main.css" rel="stylesheet" > -->
 </head>
 
 <body class="antialiased">
